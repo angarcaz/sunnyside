f1d8bdecc903a0d23517d4e0cf6da3a83771d055
angarcaz93@gmail.com <angarcaz93@gmail.com>
2023-09-06 12:33:01 +0200

    burger menu works ok

--- script.js
+++ script.js
@@ -2,5 +2,15 @@ const toggleButton = document.querySelector('.burgerNav');
 const globalNav = document.querySelector('.globalNav');
 
 toggleButton.addEventListener('click', () => {
-  globalNav.classList.toggle("open")
+  globalNav.classList.toggle('open');
+  const burgerIcon = toggleButton.querySelector('i.fas.fa-bars');
+  const xIcon = toggleButton.querySelector('span');
+  
+  if (globalNav.classList.contains('open')) {
+    burgerIcon.style.display = 'none';
+    xIcon.style.display = 'inline';
+  } else {
+    burgerIcon.style.display = 'inline';
+    xIcon.style.display = 'none';
+  }
 });
